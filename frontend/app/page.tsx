d44c32150e890270e273a8cc23c92333b0b5a6eb
Tomasz Jelito <jeli@jeli.pl>
2024-05-12 22:41:33 +0200

    add CategoryPreview

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
 import { HeroSection } from "@/components/HeroSection/HeroSection";
+import { CategoryPreview } from "@/components/CategoryPreview/CategoryPreview";
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <HeroSection />
+      <CategoryPreview />
     </main>
   );
 }
---
+++ frontend/components/CategoryPreview/CategoryPreview.tsx
@@ -0,0 +1,41 @@
+import classes from './CategoryPreview.module.css';
+import Image from 'next/image'
+import dining from './dining.png';
+import living from './living.png';
+import bedroom from './bedroom.png';
+
+
+export function CategoryPreview() {
+    return (
+        <div className={classes.component}>
+            <div className={classes.wrapper}>
+                <h2 className={classes.title}>
+                    Browse The Range
+                </h2>
+                <p className={classes.description}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <div className={classes.categories}>
+                    <div className={classes.category_card}>
+                        <Image src={dining} className={classes.category_image} alt="Dining" title='Dining' />
+                        <h3 className={classes.category_title}>
+                            Dining
+                        </h3>
+                    </div>
+                    <div className={classes.category_card}>
+                        <Image src={living} className={classes.category_image} alt="Living" title='Living' />
+                        <h3 className={classes.category_title}>
+                            Living
+                        </h3>
+                    </div>
+                    <div className={classes.category_card}>
+                        <Image src={bedroom} className={classes.category_image} alt="Bedroom" title='Bedroom' />
+                        <h3 className={classes.category_title}>
+                            Bedroom
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
