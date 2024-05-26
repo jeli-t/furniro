5707025538469b87819b75d943ce200c224e7947
jeli-t <jelitotomek@gmail.com>
2024-05-26 22:28:48 +0200

    add shop subpage

---
+++ frontend/app/shop/page.tsx
@@ -0,0 +1,15 @@
+import { PageNotFound } from "@/components/PageNotFound/PageNotFound";
+
+
+export default function Shop() {
+  return (
+    <main>
+      <PageNotFound 
+        title1="This Is Not"
+        title2="Real Shop"
+        description1="This website is only part of my portfolio."
+        description2="To contact me visit my main website."
+      />
+    </main>
+  );
+}
