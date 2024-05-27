f2df6cce483546a3d62c47d20b5f33708b2e71ff
jeli-t <jelitotomek@gmail.com>
2024-05-27 20:34:04 +0200

    make HeaderMenu responsive

--- frontend/components/HeaderMenu/HeaderMenu.tsx
+++ frontend/components/HeaderMenu/HeaderMenu.tsx
@@ -11,13 +11,31 @@ import Link from 'next/link';
 
 export function HeaderMenu() {
     return (
-        <header className={classes.header}>
-            <Link href="/">
-                <div className={classes.logo}>
-                    <Image src={logo} alt="Furniro" title='Furniro' loading='eager' />
+        <header>
+            <div className={classes.header}>
+                <Link href="/">
+                    <div className={classes.logo}>
+                        <Image src={logo} alt="Furniro" title='Furniro' loading='eager' />
+                    </div>
+                </Link>
+                <nav className={classes.nav}>
+                    <ul>
+                        <li><Link href="/">Home</Link></li>
+                        <li><Link href="/shop">Shop</Link></li>
+                        <li><Link href="/about">About</Link></li>
+                        <li><Link href="/contact">Contact</Link></li>
+                    </ul>
+                </nav>
+                <div className={classes.icons}>
+                    <ul>
+                        <li><IconUserExclamation className={classes.icon} /></li>
+                        <li><IconSearch className={classes.icon} /></li>
+                        <li><IconHeart className={classes.icon} /></li>
+                        <li><IconShoppingCart className={classes.icon} /></li>
+                    </ul>
                 </div>
-            </Link>
-            <nav className={classes.nav}>
+            </div>
+            <nav className={classes.responsive_nav}>
                 <ul>
                     <li><Link href="/">Home</Link></li>
                     <li><Link href="/shop">Shop</Link></li>
@@ -25,14 +43,6 @@ export function HeaderMenu() {
                     <li><Link href="/contact">Contact</Link></li>
                 </ul>
             </nav>
-            <div className={classes.icons}>
-                <ul>
-                    <li><IconUserExclamation className={classes.icon} /></li>
-                    <li><IconSearch className={classes.icon} /></li>
-                    <li><IconHeart className={classes.icon} /></li>
-                    <li><IconShoppingCart className={classes.icon} /></li>
-                </ul>
-            </div>
         </header>
     )
 }
